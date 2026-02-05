26efdd94a991754343bd9edadb9d6f97b65daf5b
nishant <nishantkudale04@gmail.com>
2026-02-05 13:27:50 +0530

    fix: rename FoodResponce to FoodResponse and add additional methods in FoodService interface

--- src/main/java/com/nishant/foodiesapi/service/FoodService.java
+++ src/main/java/com/nishant/foodiesapi/service/FoodService.java
@@ -1,13 +1,23 @@
 package com.nishant.foodiesapi.service;
 
 import com.nishant.foodiesapi.IO.FoodRequest;
-import com.nishant.foodiesapi.IO.FoodResponce;
-import com.nishant.foodiesapi.repository.FoodRepository;
+import com.nishant.foodiesapi.IO.FoodResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface FoodService {
     String uploadFile(MultipartFile file);
 
 
-    FoodResponce addFood(FoodRequest request , MultipartFile file);
+    FoodResponse addFood(FoodRequest request , MultipartFile file);
+
+    List<FoodResponse> readFoods();
+
+    FoodResponse readFoodById(String id);
+
+    boolean deleteFile(String filename);
+
+    void deleteFood(String id);
 }
