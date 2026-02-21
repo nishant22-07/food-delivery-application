05e4b6ee1eb0f8cbf8d70e878014516d29f32fbc
nishant <nishantkudale04@gmail.com>
2026-02-22 00:01:46 +0530

    user controller - create user api

---
+++ src/main/java/com/nishant/foodiesapi/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.nishant.foodiesapi.controller;
+
+import com.nishant.foodiesapi.IO.UserRequest;
+import com.nishant.foodiesapi.IO.UserResponse;
+import com.nishant.foodiesapi.entity.UserEntity;
+import com.nishant.foodiesapi.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+@AllArgsConstructor
+@CrossOrigin("*")
+public class UserController {
+
+    @Autowired
+    private final UserService userService;
+
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResponse registerUser(@RequestBody UserRequest userRequest) {
+        return userService.registerUser(userRequest);
+
+    }
+
+}
