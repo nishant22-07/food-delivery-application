7d637691660bcaa3a73c66056644b1e165eb5db4
nishant <nishantkudale04@gmail.com>
2026-02-21 23:46:51 +0530

    Authentication request

---
+++ src/main/java/com/nishant/foodiesapi/IO/AuthenticationRequest.java
@@ -0,0 +1,16 @@
+package com.nishant.foodiesapi.IO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthenticationRequest {
+
+    private String name;
+     private String email;
+}
