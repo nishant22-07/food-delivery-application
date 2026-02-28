da0db791f04e82f6af231a58078ffc618f219fd6
nishant <nishantkudale04@gmail.com>
2026-02-28 12:55:59 +0530

    feat: add AuthenticationFacade to abstract SecurityContext access

---
+++ src/main/java/com/nishant/foodiesapi/service/AuthenticationFacade.java
@@ -0,0 +1,8 @@
+package com.nishant.foodiesapi.service;
+
+import org.springframework.security.core.Authentication;
+
+public interface AuthenticationFacade {
+
+    Authentication getAuthentication();
+}
