71514fe92638c669f59d1a1d11e6622e0db4c6a5
nishant <nishantkudale04@gmail.com>
2026-02-28 12:57:50 +0530

    feat(cart): add CartController with add, get, remove and clear cart endpoints

---
+++ src/main/java/com/nishant/foodiesapi/controller/CartController.java
@@ -0,0 +1,59 @@
+package com.nishant.foodiesapi.controller;
+
+import com.nishant.foodiesapi.IO.CartRequest;
+import com.nishant.foodiesapi.IO.CartResponse;
+import com.nishant.foodiesapi.entity.CartEntity;
+import com.nishant.foodiesapi.repository.CartRepository;
+import com.nishant.foodiesapi.service.CartService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/cart")
+@AllArgsConstructor
+public class CartController {
+
+    private final CartService cartService;
+
+    @PostMapping
+    public CartResponse addToCart(@RequestBody CartRequest request) {
+        String foodId = request.getFoodId();
+        if(foodId == null || foodId.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "foodId not found");
+
+        }
+        return cartService.addToCart(request);
+
+    }
+
+    @GetMapping
+    public CartResponse getCart() {
+        return cartService.getCart();
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCart() {
+        cartService.clearCart();
+    }
+
+    @PostMapping("/remove")
+    public CartResponse removeFromCart(@RequestBody CartRequest request) {
+        String foodId = request.getFoodId();
+        if(foodId == null || foodId.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "foodId not found");
+        }
+
+        return cartService.removeFromCart(request);
+
+    }
+
+
+
+
+}
