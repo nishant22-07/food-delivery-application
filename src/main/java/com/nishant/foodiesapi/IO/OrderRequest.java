7f121abc30efbc9feca1b9a7acb2ed52ac19a82e
nishant <nishantkudale04@gmail.com>
2026-03-02 14:44:22 +0530

    feat(order): introduce OrderRequest model with items, address and payment details

---
+++ src/main/java/com/nishant/foodiesapi/IO/OrderRequest.java
@@ -0,0 +1,18 @@
+package com.nishant.foodiesapi.IO;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class OrderRequest {
+
+    private List<OrderItem> orderedItems;
+    private String userAddress;
+    private double amount;
+    private String email;
+    private String phoneNumber;
+    private String orderStatus;
+}
