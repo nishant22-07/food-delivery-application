9686f001834b2e2102a5fe955f0d621e4cb68a5a
nishant <nishantkudale04@gmail.com>
2026-03-02 14:52:27 +0530

    feat(order): implement OrderRepository for order retrieval and payment verification

---
+++ src/main/java/com/nishant/foodiesapi/repository/OrderRepository.java
@@ -0,0 +1,21 @@
+package com.nishant.foodiesapi.repository;
+
+import com.nishant.foodiesapi.entity.OrderEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface OrderRepository extends MongoRepository<OrderEntity, String> {
+    
+    List<OrderEntity> findByUserId(String userId);
+    Optional<OrderEntity> findByRazorpayOrderId(String razorpayOrderId);
+    
+    
+
+
+}
