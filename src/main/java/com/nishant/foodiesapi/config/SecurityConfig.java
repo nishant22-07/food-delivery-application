1b9084905c90bcdbbae47101684045bb3e831a1b
nishant <nishantkudale04@gmail.com>
2026-03-08 11:02:07 +0530

    chore(config): update application properties and CORS settings for production

--- src/main/java/com/nishant/foodiesapi/config/SecurityConfig.java
+++ src/main/java/com/nishant/foodiesapi/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
 
     private UrlBasedCorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174"));
+        config.setAllowedOrigins(List.of("https://food-delivery-application-frontend-two.vercel.app", "https://food-delivery-application-frontend-seven.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
